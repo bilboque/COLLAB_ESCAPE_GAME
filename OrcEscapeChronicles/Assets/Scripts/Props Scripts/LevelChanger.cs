e97641404c9c8698246db1c5d6dd4ec1968235ff
Leo Bonaz <leo-francois.bonaz@etu.unige.ch>
2024-06-18 15:03:17 +0200

    lvl debug

--- OrcEscapeChronicles/Assets/Scripts/Props Scripts/LevelChanger.cs
+++ OrcEscapeChronicles/Assets/Scripts/Props Scripts/LevelChanger.cs
@@ -20,24 +20,21 @@ public class LevelChanger : MonoBehaviour
             player1 = other.gameObject;
             player1Ready = true;
             player1.SetActive(false); // Deactivate player1
-            CheckBothPlayersReady();
         }
         else if (other.CompareTag("Player"))
         {
             player2 = other.gameObject;
             player2Ready = true;
             player2.SetActive(false); // Deactivate player2
-            CheckBothPlayersReady();
         }
+        if (player1Ready && player2Ready)
+            changeScene();
     }
 
-    void CheckBothPlayersReady()
+    void changeScene()
     {
-        if (player1Ready && player2Ready)
-        {
-            SceneManager.sceneLoaded += OnSceneLoaded;
-            SceneManager.LoadScene(level + 1);
-        }
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        SceneManager.LoadScene(level + 1);
     }
 
     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
