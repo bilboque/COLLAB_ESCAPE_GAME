08bec546ef05b0c23ae24f78b9df5209cb1e4b76
Leo Bonaz <leo-francois.bonaz@etu.unige.ch>
2024-06-19 18:09:24 +0200

    bridge one shot

---
+++ OrcEscapeChronicles/Assets/Scripts/Props Scripts/BridgeActiivatorLVL1.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeActiivatorLVL1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public GameObject bridge; 
+   public GameObject bridgeWalls; 
+   public GameObject openBridge;
+   public GameObject glow;
+
+   private bool isOpen = false;
+
+   void OnTriggerEnter2D(Collider2D other) 
+   {
+    isOpen = true;
+    bridge.SetActive(isOpen);
+    bridgeWalls.SetActive(isOpen);
+    openBridge.SetActive(!isOpen);
+    glow.SetActive(isOpen);
+   }
+   
+   void OnTriggerExit2D(Collider2D other) 
+   {
+    isOpen = false;
+    bridge.SetActive(isOpen);
+    bridgeWalls.SetActive(isOpen);
+    openBridge.SetActive(!isOpen);
+    glow.SetActive(isOpen);
+   }
+}
