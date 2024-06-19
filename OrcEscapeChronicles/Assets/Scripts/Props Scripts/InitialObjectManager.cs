409f5c855be25f4ac78421a65ffd4235bfb4cef9
Filipe Ramos <filipe.ramos@etu.unige.ch>
2024-06-19 10:22:50 +0200

    auto-spawner

---
+++ OrcEscapeChronicles/Assets/Scripts/Props Scripts/InitialObjectManager.cs
@@ -0,0 +1,22 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class InitialObjectManager : NetworkBehaviour
+{
+    public GameObject[] initialObjects; 
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsServer)
+        {
+            foreach (GameObject obj in initialObjects)
+            {
+                NetworkObject netObj = obj.GetComponent<NetworkObject>();
+                if (netObj != null && !netObj.IsSpawned)
+                {
+                    netObj.Spawn();
+                }
+            }
+        }
+    }
+}
