f4fd5d24f261b78a66e737f274d63ac09ba68ffb
Leo Bonaz <leo-francois.bonaz@etu.unige.ch>
2024-06-19 16:53:03 +0200

    dsajdasl

--- OrcEscapeChronicles/Assets/Scripts/Props Scripts/LevelChanger.cs
+++ OrcEscapeChronicles/Assets/Scripts/Props Scripts/LevelChanger.cs
@@ -45,8 +45,7 @@ public class LevelChanger : NetworkBehaviour
         if (NetworkManager.Singleton.IsServer)
         {
             obj.Despawn(true);
-            NetworkManager.SceneManager.UnloadScene(SceneManager.GetActiveScene());
-            NetworkManager.SceneManager.LoadScene("niveau1", LoadSceneMode.Additive);
+            NetworkManager.SceneManager.LoadScene("niveau1", LoadSceneMode.Single);
         }
     }
     
